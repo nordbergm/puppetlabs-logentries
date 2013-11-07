8f2e226f76d9971aad77b4971e719ce0c1877b57
Mattias Nordberg <mattias.nordberg@gmail.com>
2013-11-07 17:58:10 +0100

    Define logentries::follow as a type

--- manifests/follow.pp
+++ manifests/follow.pp
@@ -1,4 +1,4 @@
-class logentries::follow($log_file, $log_name) {
+define logentries::follow($log_file = $title, $log_name) {
   exec { "le_follow_${log_file}":
     command => "/usr/bin/le follow ${log_file} --name=${log_name}",
     unless => "/usr/bin/le followed ${log_file}",
