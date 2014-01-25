8673848473ac9ee7990eebbf446469187eb05f96
Mattias Nordberg <mattias.nordberg@gmail.com>
2014-01-25 22:40:50 +0100

    make sure to register before following a log file

--- manifests/follow.pp
+++ manifests/follow.pp
@@ -2,7 +2,7 @@ define logentries::follow($log_file = $title, $log_name) {
   exec { "le_follow_${log_file}":
     command => "/usr/bin/le follow ${log_file} --name=${log_name}",
     unless => "/usr/bin/le followed ${log_file}",
-    require => Package['logentries'],
+    require => [ Package['logentries'], Exec['le_register'] ],
     notify  => Service['logentries'],
   }
 }
